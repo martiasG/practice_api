c94b5f1e815b78acc3254ae563f532bb125194e8
Matias Gonzalez <matiasgonzalez@Matiass-MacBook-Pro-2.local>
2019-02-23 21:39:33 -0300

    change port 80

--- rest_service_api.py
+++ rest_service_api.py
@@ -61,4 +61,4 @@ app.add_api('swagger_mobile.yml')
     - 404 does not exist
 """
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
